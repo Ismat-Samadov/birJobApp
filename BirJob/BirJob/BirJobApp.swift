2acb63fcddc718e39c23ed7749c69aec2b1e939d
Ismat-Samadov <ismetsemedov@gmail.com>
2025-05-27 07:52:43 +0400

    init

---
+++ BirJob/BirJob/BirJobApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirJobApp.swift
+//  BirJob
+//
+//  Created by Ismat Samadov on 26.05.25.
+//
+
+import SwiftUI
+
+@main
+struct BirJobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BirJob/BirJobTests/BirJobTests.swift
@@ -0,0 +1,17 @@
+//
+//  BirJobTests.swift
+//  BirJobTests
+//
+//  Created by Ismat Samadov on 26.05.25.
+//
+
+import Testing
+@testable import BirJob
+
+struct BirJobTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
